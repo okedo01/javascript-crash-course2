39d903641e6b6cc81e6c7074fda9792c95ad0594
Eliah <eliahmwelangi01@gmail.com>
2024-11-24 14:16:25 -0800

    javascript crash course

--- To-do-list project/sandbox.js
+++ To-do-list project/sandbox.js
@@ -3,50 +3,49 @@ const listTodos = document.querySelector('.todos');
 const search = document.querySelector('.search input');
 
 const addToHTML = (todo) => {
-    let html = `
+    const html = `
         <li class="list-group-item d-flex justify-content-between align-items-center">
             <span>${todo}</span>
             <i class="fa-solid fa-trash delete"></i>
         </li>
     `;
-    listTodos.innerHTML += html;
+
+    listTodos.innerHTML += html;    
 }
 
 addForm.addEventListener('submit', e => {
 
     e.preventDefault();
-    
-    const todo = addForm.add.value.trim();
 
-    if(todo.length){
+    const todo = addForm.add.value.trim();
+    
+    if(todo.length) {
         addToHTML(todo);
         addForm.reset();
     }
+    
 })
 
-listTodos.addEventListener('click', e => {
-    if(e.target.classList.contains('delete')) {
-        e.target.parentElement.remove();
+listTodos.addEventListener('click', event => {
+    const liTag = event.target.classList.contains('delete');
+    if(liTag){
+        event.target.parentElement.remove();
     }
 })
 
-const filterTodos = (searchItem) => {
+const filteredTodos = (searchItem) => {
 
     Array.from(listTodos.children)
-        .filter((todo) => {
-            return !todo.textContent.toLocaleLowerCase().includes(searchItem);
-        })
-        .forEach((todo) => {
-            return todo.classList.add('filtered');
-        })
+        .filter((todo) => !todo.textContent.toLowerCase().includes(searchItem))
+        .forEach((todo) => todo.classList.add('filtered'))
 
     Array.from(listTodos.children)
         .filter((todo) => todo.textContent.toLowerCase().includes(searchItem))
         .forEach((todo) => todo.classList.remove('filtered'))
+    
 }
 
-// keyup events
 search.addEventListener('keyup', () => {
     const searchItem = search.value.trim().toLowerCase();
-    filterTodos(searchItem);
+    filteredTodos(searchItem);
 })
