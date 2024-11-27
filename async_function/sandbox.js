d62fecbdf6481e491afa63cf69f4dbc59488a503
Eliah <eliahmwelangi01@gmail.com>
2024-11-27 10:02:38 +0300

    Fetch API

--- async_function/sandbox.js
+++ async_function/sandbox.js
@@ -1,10 +1,10 @@
-console.log(1);
-console.log(2);
+// console.log(1);
+// console.log(2);
 setTimeout(() => {
-    console.log('async function fired');
+    // console.log('async function fired');
 }, 2000)
-console.log(3);
-console.log(4);
+// console.log(3);
+// console.log(4);
 
 
 const getSomething = () => {
@@ -22,3 +22,36 @@ getSomething()
         console.log(error);
     })
 
+
+// fetch('todos/luigi.json')
+//     .then(response => {
+//         console.log("resolved", response);
+//         return response.json();
+//     }).then(data => {
+//         console.log(data);
+//     }).catch(error => {
+//         console.log("rejected", error);
+//     })
+
+
+// Async & Await
+const getData = async () => {
+    const response = await fetch('todos/luigiss.json');
+    
+    if(response.status !== 200) {
+        throw new Error("cannot fetch data");
+    }
+    
+    const data = await response.json();
+    return data;
+}
+
+console.log(1);
+console.log(2);
+
+getData()
+    .then(data => console.log("resolved: ", data))
+    .catch(error => console.log("rejected: ", error.message));
+
+console.log(3);
+console.log(4);
