503f0dfa2c32924b87861d3b1574dbc5a697ebe2
Eliah <eliahmwelangi01@gmail.com>
2024-11-29 13:59:32 +0300

    update day/night & icons

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -1,6 +1,8 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
 
@@ -20,6 +22,20 @@ const updateUI = (data) => {
             <span>&deg;C</span>
         </div>
     `;
+
+    // update day/night & icons
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = '';
+    
+    if(weather.IsDayTime) {
+        timeSrc = 'img/day.svg';
+    } else {
+        timeSrc = 'img/night.svg';
+    }
+
+    time.setAttribute('src', timeSrc);
 
     // remove d-none class if present
     if(card.classList.contains('d-none')) {
