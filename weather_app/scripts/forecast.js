6c6f1e7b63dc6127aff5d86ba31ca294125af05b
Eliah <eliahmwelangi01@gmail.com>
2024-11-29 14:28:26 +0300

    ternary operator

--- weather_app/scripts/forecast.js
+++ weather_app/scripts/forecast.js
@@ -21,10 +21,3 @@ const getCity = async (city) => {
 
     return data[0];
 }
-
-// getCity('Dodoma')
-//     .then(data => getWeather(data.Key))
-//     .then(data => console.log(data))
-//     .catch(error => console.log(error));
-
-
